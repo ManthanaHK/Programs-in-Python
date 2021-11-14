dbd53a3c339c665a0afdd6e02b2b5aac96c18430
Manthana H K <89962104+ManthanaHK@users.noreply.github.com>
2021-11-13 21:37:45 -0800

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -11,3 +11,16 @@ def palindrome(word_num):
 print('Enter a text or number to findout whether it is palindrome or not:')
 word_num = input()
 palindrome(word_num)   
+
+//*******Another Way***********//
+
+reverse_list_word = []
+print("Enter the word:",end =' ')
+word = input()
+list_word = list(word)
+for i in range(1,int(len(list_word) + 1)):
+    reverse_list_word.append(list_word[-i])
+if list_word == reverse_list_word:
+    print("Entered word {}, is Palindrome".format(word))
+else:
+    print("Entered word {}, is not palindrome".format(word))
